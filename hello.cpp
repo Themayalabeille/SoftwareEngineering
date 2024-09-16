4e8822dd5b85db7d04c44d7587ec26395942dc80
Cyril Carlier <carlier36u@etu.univ-lorraine.fr>
2024-09-16 15:24:21 +0200

    add hello_world with CMake and VersionConfig

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+#include <HelloProjConfig.h>
+#include <iostream>
+
+
+void hello_world() {
+	std::cout << "Hello world!\n " << std::endl;
+}
+
+void print_version() {
+	std::cout << "Project version is " << HelloProj_VERSION_MAJOR << "." << HelloProj_VERSION_MINOR << std::endl;
+}
+
+int main() {
+	hello_world();
+	print_version();
+	
+}
+
